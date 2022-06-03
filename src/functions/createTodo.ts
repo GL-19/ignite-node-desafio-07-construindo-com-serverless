78bf240d44bf7345b5a3398bd05ef39e6493d8e2
GL-19 <loiolagl19@gmail.com>
2022-06-03 12:04:55 -0300

    Primeiro commit

---
+++ src/functions/createTodo.ts
@@ -0,0 +1,46 @@
+import { APIGatewayProxyHandler } from "aws-lambda";
+import { document } from "../utils/dynamodbClient";
+import { v4 as uuid } from "uuid";
+
+interface IRequestBody {
+	title: string;
+	deadline: string;
+}
+
+export const handler: APIGatewayProxyHandler = async (event) => {
+	const { title, deadline } = JSON.parse(event.body) as IRequestBody;
+	const { id: user_id } = event.pathParameters;
+
+	const id = uuid();
+
+	await document
+		.put({
+			TableName: "todos",
+			Item: {
+				id,
+				user_id,
+				title,
+				done: false,
+				deadline: new Date(deadline),
+			},
+		})
+		.promise();
+
+	const response = await document
+		.query({
+			TableName: "todos",
+			KeyConditionExpression: "id = :id",
+			ExpressionAttributeValues: {
+				":id": id,
+			},
+		})
+		.promise();
+
+	return {
+		statusCode: 201,
+		body: JSON.stringify({
+			message: "Todo criado com sucesso!",
+			todo: response,
+		}),
+	};
+};
